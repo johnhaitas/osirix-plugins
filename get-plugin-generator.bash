b7c4108432279ee81668d5d2fa9d33ed85ff0675
John Haitas <john@haitas.com>
2011-01-19 19:41:34 +0000

    updating script to get plugin generator to use svn export rather than checkout

--- get-plugin-generator.bash
+++ get-plugin-generator.bash
@@ -1,9 +1,9 @@
 #!/bin/bash
 # To get Osirix Plugin Generator
 
-svn co https://osirixplugins.svn.sourceforge.net/svnroot/osirixplugins/_help
-cd _help
+svn export https://osirixplugins.svn.sourceforge.net/svnroot/osirixplugins/_help
+pushd _help
 unzip Osirix\ Plugin\ Generator.zip
 mv Osirix\ Plugin\ Generator.app ../
-cd ../
+popd
 rm -rf _help
